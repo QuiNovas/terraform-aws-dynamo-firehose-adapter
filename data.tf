d3cb4cf33ef29cf92cb8ad01eca54b19140b93e2
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-03-17 12:12:13 -0500

    Initial checkin

---
+++ data.tf
@@ -0,0 +1,24 @@
+data "aws_iam_policy_document" "dynamo_firehose_adapter" {
+  statement {
+    actions   = [
+      "dynamodb:DescribeStream",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:ListStreams",
+    ]
+    resources = [
+      "${var.dynamodb_stream_arn}",
+    ]
+    sid       = "AllowReadingFromDynamoDBStream"
+  }
+
+  statement {
+    actions   = [
+      "firehose:PutRecordBatch",
+    ]
+    resources = [
+      "${var.kinesis_firehose_arn}",
+    ]
+    sid       = "AllowWritingToFirehose"
+  }
+}
