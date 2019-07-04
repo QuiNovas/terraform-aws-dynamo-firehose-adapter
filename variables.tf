46d87a2bc784b459d166989e2228f68158ba6d75
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-04 19:11:30 +0530

    updated to terraform 12

--- variables.tf
+++ variables.tf
@@ -1,41 +1,42 @@
 variable "batch_size" {
   default     = 100
   description = "The largest number of records that Lambda will retrieve from the DynamoDB stream at then time of invocation"
-  type        = "string"
+  type        = string
 }
 
 variable "dead_letter_arn" {
   description = "The arn for the SNS topic that handles dead letters"
-  type        = "string"
+  type        = string
 }
 
 variable "dynamodb_image_type" {
   description = "Can either be NewImage or OldImage. This corresponds to the records in the DynamoDB Stream"
-  type        = "string"
+  type        = string
 }
 
 variable "dynamodb_stream_arn" {
   description = "The ARN of the Table Stream to connect to the adapter"
-  type        = "string"
+  type        = string
 }
 
 variable "kinesis_firehose_arn" {
   description = "The ARN of the Kinesis Firehose to deliver the DynamoDB records to"
-  type        = "string"
+  type        = string
 }
 
 variable "kinesis_firehose_name" {
   description = "The name of the Kinesis Firehose to deliver the DynamoDB records to"
-  type        = "string"
+  type        = string
 }
 
 variable "kms_key_arn" {
   description = "The arn of the KMS key used to encrypt the environment variables"
-  type        = "string"
+  type        = string
 }
 
 variable "name_prefix" {
   default     = ""
   description = "The prefix to place on all created resources"
-  type        = "string"
+  type        = string
 }
+
